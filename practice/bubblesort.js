fca579a2f62ad14054d69db3ab94f9590e9b04bf
pragatisharma21 <pragatis212@gmail.com>
2025-01-28 15:33:59 +0530

    selection sort prectice

--- practice/bubblesort.js
+++ practice/bubblesort.js
@@ -1,13 +1,33 @@
-let arr = [5, 3, 8, 4, 2];
+ let arr = [5, 3, 8, 4, 2];
 
-for (let i = 0; i < arr.length - 1; i++) {  // Total passes
-    for (let j = 0; j < arr.length - i - 1; j++) {  // Adjacent compare
-        if (arr[j] > arr[j + 1]) {  // Agar wrong order hai, swap karo
-            let temp = arr[j];
-            arr[j] = arr[j + 1];
-            arr[j + 1] = temp;
+// for (let i = 0; i < arr.length - 1; i++) {  // Total passes
+//     for (let j = 0; j < arr.length - i - 1; j++) {  // Adjacent compare
+//         if (arr[j] > arr[j + 1]) {  // Agar wrong order hai, swap karo
+//             let temp = arr[j];
+//             arr[j] = arr[j + 1];
+//             arr[j + 1] = temp;
+//         }
+//     }
+// }
+
+// console.log("Bubble Sort Result: ", arr);
+// for(let i = 0; i<arr.length; i++){
+//     for(j=0; j<arr.length - i -1; j++){
+//         if(arr[j] > arr[j+1]){
+//             let temp = arr[j]
+//             arr[j] = arr[j+1]
+//             arr[j+1] = temp 
+//         }
+//     }
+// }
+// console.log(arr)
+for(let i = 0; i<arr.length; i++){
+    for(let j = 0; j<arr.length-i-1; j++){
+        if(arr[j] > arr[j+1]){
+            let temp = arr[j]
+            arr[j] = arr[j+1]
+            arr[j+1] = temp
         }
     }
 }
-
-console.log("Bubble Sort Result: ", arr);
+console.log(arr)
---
+++ practice/selectionsort.js
@@ -0,0 +1,13 @@
+arr = [5, 3, 8, 4, 2]
+for(i = 0; i<arr.length-1 ; i++){
+    let minIndex = i
+    for(let j = i+1; j<arr.length; j++){
+        if(arr[j] < arr[minIndex]){
+            minIndex = j
+        }
+    }
+    let temp = arr[i]
+    arr[i] = arr[minIndex]
+    arr[minIndex] = temp
+}
+console.log(arr)
