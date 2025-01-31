2692c673a9fa5ffef7d496cea5c9926341f15118
pragatisharma21 <pragatis212@gmail.com>
2025-01-31 15:54:03 +0530

    new dsa question is done

---
+++ practice/normalprec.js
@@ -0,0 +1,18 @@
+// n = 12345 /// is ka count 5 h ye tumko or mujhklo dono ko pata hai theek h
+// let count = 0
+// while(n>0){
+//     count++
+//     n = Math.floor(n/10)
+// }
+// console.log(count)
+
+
+
+n = 123456789
+let count = 0
+while( n> 0){
+    count++
+    n = Math.floor(n/10)
+
+}
+console.log(count)
--- practice/selectionsort.js
+++ practice/selectionsort.js
@@ -1,13 +1,32 @@
-arr = [5, 3, 8, 4, 2]
-for(i = 0; i<arr.length-1 ; i++){
+// arr = [5, 3, 8, 4, 2]
+// for(i = 0; i<arr.length-1 ; i++){
+//     let minIndex = i
+//     for(let j = i+1; j<arr.length; j++){
+//         if(arr[j] < arr[minIndex]){
+//             minIndex = j
+//         }
+//     }
+//     let temp = arr[i]
+//     arr[i] = arr[minIndex]
+//     arr[minIndex] = temp
+// }
+// console.log(arr)
+arr = [13, 46, 24, 52, 20, 9]
+for(let i = 0; i<arr.length-1; i++){
     let minIndex = i
-    for(let j = i+1; j<arr.length; j++){
+    for(let j = i ; j< arr.length; j++){
         if(arr[j] < arr[minIndex]){
             minIndex = j
+
         }
+            
     }
     let temp = arr[i]
     arr[i] = arr[minIndex]
     arr[minIndex] = temp
 }
 console.log(arr)
+// for(i = 0; i< arr.length-1; i++){
+
+// }
+
