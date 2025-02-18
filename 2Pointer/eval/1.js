88f959371211e43f410452b5147454cc4bc5f2ed
pragatisharma21 <pragatis212@gmail.com>
2025-02-19 00:53:49 +0530

    codeee

--- 2Pointer/eval/1.js
+++ 2Pointer/eval/1.js
@@ -1,16 +1,21 @@
-function solve(N, S, L, R) {
-  let arr = S.split("");
-  while (L <=
-     R) {
-    let temp = arr[L];
-    arr[L] = arr[R];
-    arr[R] = temp;
-    L++;
-    R--;
-  }
-  console.log(arr.join(""));
+// function solve(N, S, L, R) {
+//   let arr = S.split("");
+//   while (L <=
+//      R) {
+//     let temp = arr[L];
+//     arr[L] = arr[R];
+//     arr[R] = temp;
+//     L++;
+//     R--;
+//   }
+//   console.log(arr.join(""));
+// }
+// let N = 8;
+// let S = "fxavixem";
+// let L = 0;
+// let R = 1;
+arr = [1,2,3,4]
+
+for(i = arr.length-1 ; i>=0; i--){
+  console.log(arr[i])
 }
-let N = 8;
-let S = "fxavixem";
-let L = 0;
-let R = 1;
--- 2Pointer/eval/2.js
+++ 2Pointer/eval/2.js
@@ -5,10 +5,10 @@ function maxApples(weights, r) {
     // Use a Set to track picked apples
     const picked = new Set();
 
-    // Iterate through the sorted array
+    // Iterate through the sorted array [2,3,4,5,6,10]
     for (const weight of weights) {
         // Check if neither weight/r nor weight*r exists in the set
-        if (!picked.has(weight / r) && !picked.has(weight * r)) {
+        if (!picked.has(weight / r) ) {
             // Add the weight to the set
             picked.add(weight);
         }
@@ -19,6 +19,6 @@ function maxApples(weights, r) {
 }
 
 // Example usage:
-const weights = [1 ,2, 3, 4, 5];
-const r = 1; // Dynamic value of r
+const weights = [2, 3 ,6, 5, 4 ,10];
+const r = 2; // Dynamic value of r
 console.log(maxApples(weights, r)); // Output: 3
