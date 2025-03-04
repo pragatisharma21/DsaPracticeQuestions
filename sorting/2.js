cfb7174ad248566eff1d0d832fcd06c4550d69cb
pragatisharma21 <pragatis212@gmail.com>
2025-03-04 12:22:12 +0530

    ll

--- sorting/2.js
+++ sorting/2.js
@@ -1,16 +1,20 @@
-let arr = [1,2,4,5,8] 
-let n = arr.length
+function missingNumber(arr) {
+  if (arr.length === 1 && arr[0] === 1) {
+    return 2;
+  }
+  if (arr.length === 1 && arr[0] > 1) {
+    return arr[0] - 1;
+  }
+  arr.sort((a, b) => a - b);
+  if (arr[0] > 1) {
+    return arr[0] - 1;
+  }
 
-for(let i = 0; i<n-1; i++){
-    let minindex = i
-    for(let j = i+1; j<n; j++ ){
-        if(arr[j] < arr[minindex]){
-            minindex = j
-        }
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] - arr[i - 1] > 1) {
+      return arr[i] - 1;
     }
-    let temp = arr[i]
-    arr[i] = arr[minindex]
-    arr[minindex] = temp
-}
+  }
 
-console.log(arr)
+  return arr[arr.length - 1] + 1;
+}
